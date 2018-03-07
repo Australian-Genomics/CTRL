250e0a5cf8e85639078705df4850938e7bb950e4
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-07 19:29:40 +0530

    rubocop fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { FactoryGirl.create(:user)}
+  let(:user) { FactoryGirl.create(:user) }
 
-  it {expect(user.email).to eq('test1@email.com')}
-  it {expect(user.password).to eq('password')}
+  it { expect(user.email).to eq('test1@email.com') }
+  it { expect(user.password).to eq('password') }
 end
