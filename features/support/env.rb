1a2962df2a92422a13cdcbeaa4ceabd4a9e7e9ed
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-07 19:23:37 +0530

    BDD tests for users

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,11 @@
 
 require 'cucumber/rails'
 
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.default_driver = :selenium
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
