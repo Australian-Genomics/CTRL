b161adddea710b5d43ea642e36293faa68ad1fc8
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-09 11:26:08 +0530

    Deploying to heroku from circleci

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+git remote add heroku https://git.heroku.com/agha-canary.git
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+mkdir -p /usr/local/lib /usr/local/bin
+tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_EMAIL
+  password $HEROKU_PASS
+machine git.heroku.com
+  login $HEROKU_EMAIL
+  password $HEROKU_PASS
+EOF
