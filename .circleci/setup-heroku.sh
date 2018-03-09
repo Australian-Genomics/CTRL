0741379728fb4175de5e8538f1ad6b69057b536f
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-09 14:51:32 +0530

    Trying with heroku auth token

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -7,8 +7,8 @@ ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_EMAIL
-  password $HEROKU_API_KEY
+  password $HEROKU_TOKEN
 machine git.heroku.com
   login $HEROKU_EMAIL
-  password $HEROKU_API_KEY
+  password $HEROKU_TOKEN
 EOF
