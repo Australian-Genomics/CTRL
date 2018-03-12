7e40bd12fe995c13382e89aa8fdff20a0da92c84
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-12 12:19:53 +0530

    fixed the warning for factory girl by upgrading it to factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryBot.create(:user) }
 
   it { expect(user.email).to eq('test1@email.com') }
   it { expect(user.password).to eq('password') }
