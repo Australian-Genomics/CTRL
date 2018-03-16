92a0fe122d5d4de8869865bdb6eb145854ee770b
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-16 08:56:48 +0530

    Rubocop fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :middle_name, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = %i[first_name middle_name email password password_confirmation remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
