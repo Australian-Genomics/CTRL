5c9ec64b842ffb7f47906730627b3ec3d42fa555
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-17 03:39:03 +0530

    Removed the user edit details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,5 @@
 class UsersController < ApplicationController
   def create
-    @user = User.new(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params)
-      redirect_to user_path
-    else
-      render_status :error
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :family_name, :middle_name, :dob, :email, :address, :phone_no, :preferred_contact_method)
+    @user = User.new(params[:user])
   end
 end
