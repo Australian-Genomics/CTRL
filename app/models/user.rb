ef7d24f484265f384f688daa448bafbbf1269936
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-20 21:07:22 +0530

    Added flagship names to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,21 @@ class User < ApplicationRecord
 
   validates :first_name, :family_name, presence: true, allow_blank: false
 
-  enum flagship: ['Acute Care Genomic Testing', 'Acute Lymphoblastic Leukaemia']
+  enum flagship: ['Acute Care Genomic Testing',
+                  'Acute Lymphoblastic Leukaemia',
+                  'Brain Malformations',
+                  'Cardiovascular Genetic Disorders',
+                  'chILDRANZ',
+                  'Epileptic Encephalopathy',
+                  'Genetic Immunology',
+                  'Genomic Autopsy',
+                  'Hereditary Cancer Syndromes (ICCon)',
+                  'HIDDEN Renal Genetics',
+                  'Intellectual Disability',
+                  'Leukodystrophies',
+                  'Mitochondrial Disorders',
+                  'Neuromuscular Disorders',
+                  'Renal Genetic Disorders',
+                  'Solid Tumours']
+
 end
