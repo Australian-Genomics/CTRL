c7aa937050c195fbf7afeca8a9e783445436a383
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-21 15:10:05 +0530

    Added the route and view for login confirmation request

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,11 @@
+class PasswordsController < Devise::PasswordsController
+  def sent
+
+  end
+
+  protected
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    passwords_sent_path
+  end
+end
