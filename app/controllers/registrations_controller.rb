816dc396501354e904945eff68982b478a4e41ef
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-23 16:38:09 +0530

    Rubocop fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     step_one_path
   end
 end
