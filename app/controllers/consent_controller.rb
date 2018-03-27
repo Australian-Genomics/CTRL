4c8a6c200dddcf167b8f5e7eb54a32450f58ed12
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-28 01:51:11 +0530

    updating the user params with current_consent_step enum type

--- app/controllers/consent_controller.rb
+++ app/controllers/consent_controller.rb
@@ -1,13 +1,24 @@
 class ConsentController < ApplicationController
   before_action :authenticate_user!
 
-  def step_one; end
+  def step_one
+    if current_user.update(current_consent_step: 0)
+      render 'step_one.html.erb'
+    else
+      redirect_to :back
+    end
+  end
 
-  def step_two; end
+  def step_two
+  end
 
-  def step_three; end
+  def step_three;
+  end
 
-  def confirm_answers; end
+  def confirm_answers;
+  end
 
-  def review_answers; end
+  def review_answers;
+  end
+
 end
