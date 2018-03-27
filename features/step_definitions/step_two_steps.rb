ec69fff53dedb6d73b33597dcc6806b8b0725b22
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-27 14:14:05 +0530

    rubocop fix

--- features/step_definitions/step_two_steps.rb
+++ features/step_definitions/step_two_steps.rb
@@ -6,20 +6,20 @@ When('I click on Save and Exit') do
   click_link('Save and Exit')
 end
 
-When("I click on Back") do
+When('I click on Back') do
   click_link('Back')
 end
 
-When("I click on Select All") do
-  find("#step_two_form > div.d-flex.text-right.mt-3.mb-2.pr-3 > label > span").click
+When('I click on Select All') do
+  find('#step_two_form > div.d-flex.text-right.mt-3.mb-2.pr-3 > label > span').click
 end
 
-When("I click on Unselect All") do
+When('I click on Unselect All') do
   find(:xpath, "//*[@id='step_two_form']/div[4]/label/span").click
 end
 
-When("I click on Next button") do
-  find("#step_two_form > div.row.flex-md-row > div.col-6.col-md-3.order-1.order-md-2.d-flex.my-15.my-md-30 > input").click
+When('I click on Next button') do
+  find('#step_two_form > div.row.flex-md-row > div.col-6.col-md-3.order-1.order-md-2.d-flex.my-15.my-md-30 > input').click
 end
 
 Then('I should see the step one of consent') do
@@ -41,10 +41,10 @@ Then('I should see the dashboard page') do
   expect(page).to have_content('logged in')
 end
 
-Then("I should see the confirm answers page") do
+Then('I should see the confirm answers page') do
   expect(page).to have_content('Confirm')
 end
 
-Then("I should see the review answers page") do
-  expect(page).to have_content("Review Answers")
+Then('I should see the review answers page') do
+  expect(page).to have_content('Review Answers')
 end
