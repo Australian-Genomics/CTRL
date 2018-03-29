94154665c57499a6a711a1b7ce2f2dda04a92105
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-29 14:36:28 +0530

    Rubocop fix

--- app/controllers/consent_controller.rb
+++ app/controllers/consent_controller.rb
@@ -26,12 +26,9 @@ class ConsentController < ApplicationController
     end
   end
 
-  def step_four;
-  end
+  def step_four; end
 
-  def confirm_answers;
-  end
+  def confirm_answers; end
 
-  def review_answers;
-  end
+  def review_answers; end
 end
