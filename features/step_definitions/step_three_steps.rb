7a172fd142eafb91b501834693c43dcc4a49b0c6
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-03-29 14:33:19 +0530

    Cucumber tests for step 3

---
+++ features/step_definitions/step_three_steps.rb
@@ -0,0 +1,11 @@
+When("I click Next") do
+  click_button('Next')
+end
+
+Then("I should be on the step three page of consent") do
+  expect(page).to have_content('Agreement to participate in AGHA research')
+end
+
+Then("I should see the step four of the consent") do
+  expect(page).to have_content('step 4')
+end
