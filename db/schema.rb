4ed31d01faf19fafb536e77e45d69ec601b6ad2f
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-04 23:15:53 +0530

    Added child attributes to the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_03_074607) do
+ActiveRecord::Schema.define(version: 2018_04_04_173630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 2018_04_03_074607) do
     t.string "kin_email"
     t.integer "current_consent_step"
     t.string "study_id"
+    t.string "child_first_name"
+    t.string "child_middle_name"
+    t.string "child_family_name"
+    t.date "child_dob"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
