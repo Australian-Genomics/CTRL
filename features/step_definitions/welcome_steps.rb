41a94ad024fb1764321b320f1e225e3d5a175f2b
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-05 09:34:42 +0530

    rubocop fix

--- features/step_definitions/welcome_steps.rb
+++ features/step_definitions/welcome_steps.rb
@@ -1,8 +1,8 @@
-When("I click on Log Out") do
-  click_link("Log Out")
+When('I click on Log Out') do
+  click_link('Log Out')
 end
 
-Then("I should be signed out") do
+Then('I should be signed out') do
   expect(page).to have_content('Register Now')
   expect(page).to have_content('Log in')
   expect(page).to_not have_content('Log Out')
