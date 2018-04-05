39ccf1b78b1d0aba89f38e44c78496a6de8af02e
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-05 10:45:32 +0530

    Added js for onchange and load event while editing the user details

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.datepicker').datepicker();
 });
+
+$('#user_is_parent').change(function () {
+    if ($(this).is(":checked")) {
+        $('#kin').hide();
+        $('#child').show();
+    } else {
+        $('#kin').show();
+        $('#child').hide();
+    }
+})
+
+$(window).on('load', function () {
+    if ($('#user_is_parent').is(":checked")) {
+        $('#kin').hide();
+        $('#child').show();
+    } else {
+        $('#kin').show();
+        $('#child').hide();
+    }
+})
