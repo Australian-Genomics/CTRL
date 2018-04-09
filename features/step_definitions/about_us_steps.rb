ebe691a23f40cdeb1aaab458008b7e7f6551feeb
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-09 12:56:06 +0530

    Added tests for dashboard page

---
+++ features/step_definitions/about_us_steps.rb
@@ -0,0 +1,19 @@
+When("I click on My Personal Details") do
+  click_link('My Personal Details')
+end
+
+When("I click on My Activities") do
+  click_link('My Activities')
+end
+
+Then("I click on About") do
+  click_link('About')
+end
+
+Then("I should see an introduction about the platform") do
+  expect(page).to have_content('Introduction to this platform')
+end
+
+Then("I should see the personal details page") do
+  expect(page).to have_content('My Personal Details')
+end
