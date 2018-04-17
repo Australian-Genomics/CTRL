7dac9505d554589aa5ee721716c2287fa303d0ba
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-17 11:08:07 +0530

    commented out the code, need to ask Mo how we can refactor this

--- app/controllers/consent_controller.rb
+++ app/controllers/consent_controller.rb
@@ -29,21 +29,21 @@ class ConsentController < ApplicationController
 
   def goto_step
     # if current_user.update(current_consent_step: params[:registration_step_two].to_i)
-      if params[:registration_step_two]
-        case (params[:registration_step_two].to_i)
-        when 1
-          render 'step_two.html.erb'
-        when 2
-          render 'step_three.html.erb'
-        when 3
-          render 'step_four.html.erb'
-        when 4
-          render 'step_five.html.erb'
-        end
-      elsif params[:save]
-        current_user.update(current_consent_step: 4)
-        redirect_to dashboard_index_path
-      end
+    #   if params[:registration_step_two]
+    #     case (params[:registration_step_two].to_i)
+    #     when 1
+    #       render 'step_two.html.erb'
+    #     when 2
+    #       render 'step_three.html.erb'
+    #     when 3
+    #       render 'step_four.html.erb'
+    #     when 4
+    #       render 'step_five.html.erb'
+    #     end
+    #   elsif params[:save]
+    #     current_user.update(current_consent_step: 4)
+    #     redirect_to dashboard_index_path
+    #   end
   end
 
   def confirm_answers;
