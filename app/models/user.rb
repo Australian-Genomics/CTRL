c1d29174c71d5c7e73fd5ac07e7102674a0294e3
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-17 11:08:50 +0530

    updated the user and step model for saving the steps and the answer

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :family_name, :flagship, :study_id
   has_many :steps, dependent: :destroy
+  after_save :create_consent_step
 
   enum flagship: ['Acute Care Genomic Testing',
                   'Acute Lymphoblastic Leukaemia',
@@ -24,4 +25,11 @@ class User < ApplicationRecord
                   'Renal Genetic Disorders',
                   'Solid Tumours']
 
+  def create_consent_step
+    self.steps.create(number: 1,accepted: false)
+    self.steps.create(number: 2,accepted: false)
+    self.steps.create(number: 3,accepted: false)
+    self.steps.create(number: 4,accepted: false)
+    self.steps.create(number: 5,accepted: false)
+  end
 end
