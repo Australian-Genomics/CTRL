499bca782c22e0f67d1900bbaea4283e3198493a
Manu Dwivedi <manu@curvetomorrow.com.au>
2018-04-27 23:25:09 +0530

    Bug fix: use turbolinks:load instead of page:load event

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require jquery3
 //= require popper
 //= require bootstrap
-//= require_tree .
 //= require jquery-ui
+//= require_tree .
