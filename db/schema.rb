01876c8838707ab935b5df773231db95d8e9e710
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-04-27 10:47:05 +0530

    Updated Schema file after the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_17_135450) do
+ActiveRecord::Schema.define(version: 2018_04_26_112340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
     t.integer "number"
-    t.boolean "answer"
+    t.integer "answer", default: 0
     t.bigint "step_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "question_id"
+    t.bigint "user_id"
     t.index ["step_id"], name: "index_questions_on_step_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "steps", force: :cascade do |t|
