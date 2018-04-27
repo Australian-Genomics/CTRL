ee455bc774417269f318bf98bbdb6d944e0dad19
Manu Dwivedi <manu@curvetomorrow.com.au>
2018-04-28 00:50:34 +0530

    Add rails ujs back in because data-method stops working on anchor tags

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require jquery3
