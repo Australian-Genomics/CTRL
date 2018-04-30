e23eb63162528b903e1a1acaf8ed791c3f270d84
Manu Dwivedi <manu@curvetomorrow.com.au>
2018-05-01 04:36:11 +0530

    Add tests for step model. Remove unused spec file

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,21 +2,21 @@ class Step < ApplicationRecord
   has_many :questions, dependent: :destroy
   accepts_nested_attributes_for :questions
 
-  def build_question_for_step(step, user_id)
-    range_of_values_for(step).each do |time|
+  def build_question_for_step(user_id)
+    range_of_values_for(number).each do |time|
       questions.build(question_id: time, user_id: user_id)
     end
   end
 
   def range_of_values_for(step)
     case step
-    when :two
+    when 2
       (1..11)
-    when :three
+    when 3
       (12..14)
-    when :four
+    when 4
       (15..20)
-    when :five
+    when 5
       (21..31)
     end
   end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Step, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create :user }
+  let(:step_two) { FactoryBot.create :step, number: 2 }
+  let(:step_three) { FactoryBot.create :step, number: 3 }
+  let(:step_four) { FactoryBot.create :step, number: 4 }
+  let(:step_five) { FactoryBot.create :step, number: 5 }
+
+  describe '#build_question_for_step' do
+    it 'should build 11 questions for step_two' do
+      expect(step_two.questions).to receive(:build).exactly(11).times
+      expect(step_two.build_question_for_step(user.id)).to eq 1..11
+    end
+
+    it 'should build 3 questions for step_three' do
+      expect(step_three.questions).to receive(:build).exactly(3).times
+      expect(step_three.build_question_for_step(user.id)).to eq 12..14
+    end
+
+    it 'should build 6 questions for step_four' do
+      expect(step_four.questions).to receive(:build).exactly(6).times
+      expect(step_four.build_question_for_step(user.id)).to eq 15..20
+    end
+
+    it 'should build 11 questions for step_five' do
+      expect(step_five.questions).to receive(:build).exactly(11).times
+      expect(step_five.build_question_for_step(user.id)).to eq 21..31
+    end
+  end
 end
