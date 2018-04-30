fecdb9053b85692ae8ec6d6edbee108258429eee
Manu Dwivedi <manu@curvetomorrow.com.au>
2018-05-01 05:00:37 +0530

    Spec fix: expect the correct questions built

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,67 +43,62 @@ RSpec.describe User, type: :model do
     end
 
     it 'should have step one' do
-      expect(user.steps[0].number).to eq(1)
-      expect(user.steps[0].accepted).to be false
-      expect(user.steps[0].questions).to be_blank
+      expect(user.step_one.number).to eq(1)
+      expect(user.step_one.accepted).to be false
+      expect(user.step_one.questions).to be_blank
     end
 
     it 'should have step two' do
-      expect(user.steps[1].number).to eq(2)
-      expect(user.steps[1].accepted).to be false
-      expect(user.steps[1].questions).to be_blank
+      expect(user.step_two.number).to eq(2)
+      expect(user.step_two.accepted).to be false
+      expect(user.step_two.questions).to be_blank
     end
 
     it 'should have step three' do
-      expect(user.steps[2].number).to eq(3)
-      expect(user.steps[2].accepted).to be false
-      expect(user.steps[2].questions).to be_blank
+      expect(user.step_three.number).to eq(3)
+      expect(user.step_three.accepted).to be false
+      expect(user.step_three.questions).to be_blank
     end
 
     it 'should have step four' do
-      expect(user.steps[3].number).to eq(4)
-      expect(user.steps[3].accepted).to be false
-      expect(user.steps[3].questions).to be_blank
+      expect(user.step_four.number).to eq(4)
+      expect(user.step_four.accepted).to be false
+      expect(user.step_four.questions).to be_blank
     end
 
     it 'should have step five' do
-      expect(user.steps[4].number).to eq(5)
-      expect(user.steps[4].accepted).to be false
-      expect(user.steps[4].questions).to be_blank
+      expect(user.step_five.number).to eq(5)
+      expect(user.step_five.accepted).to be false
+      expect(user.step_five.questions).to be_blank
     end
   end
 
   describe '#step_one' do
     it 'should return step with number 1' do
-      expect(user.step_one).to eq(user.steps[0])
       expect(user.step_one.number).to eq 1
     end
   end
 
   describe '#step_two' do
     it 'should return step with number 2' do
-      expect(user.step_two).to eq(user.steps[1])
       expect(user.step_two.number).to eq 2
     end
   end
 
   describe '#step_three' do
     it 'should return step with number 3' do
-      expect(user.step_three).to eq(user.steps[2])
       expect(user.step_three.number).to eq 3
     end
   end
 
   describe '#step_four' do
     it 'should return step with number 4' do
-      expect(user.step_four).to eq(user.steps[3])
       expect(user.step_four.number).to eq 4
     end
   end
 
   describe '#step_five' do
     it 'should return step with number 5' do
-      expect(user.step_five).to eq(user.steps[4])
       expect(user.step_five.number).to eq 5
     end
   end
