2016d6a90528ecf0e21bbcd1e22fe465be089b9e
mohjay <mohinder@curvetomorrow.com.au>
2018-05-01 22:45:47 +0800

    setting up data and sending reminders using delayed job

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+  def send_first_survey_email(user)
+    @user = user
+
+    sender = 'ctrl@australiangenomics.org.au'
+    user.update(survey_one_email_sent: true)
+    mail(:to => "#{user.first_name} #{user.family_name} <#{user.email}>", :subject => "Australian Genomics Rare Disease Patient Survey – PART 1", sender: sender, from: sender)
+  end
+end
