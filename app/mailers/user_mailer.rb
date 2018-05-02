43c5e7b1bdfe9145d1dcd9be5cb233ccc5b6f25b
mohjay <mohinder@curvetomorrow.com.au>
2018-05-02 19:19:13 +0800

    added images to html email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,9 @@ class UserMailer < ApplicationMailer
   def send_first_survey_email(user)
     @user = user
     @genetic_counsellor = @user.genetic_counsellor
+    attachments.inline['logo@2x.png'] = File.read(File.join(Rails.root, "app", "assets", "images", "logo@2x.png"))
+    attachments.inline['Rectangle.png'] = File.read(File.join(Rails.root, "app", "assets", "images", "Rectangle.png"))
+    attachments.inline['Group.png'] = File.read(File.join(Rails.root, "app", "assets", "images", "Group.png"))
 
     sender = 'ctrl@australiangenomics.org.au'
 
