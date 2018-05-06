8de53d80c340d28d7f9fcca04ea9221a039b7853
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-05-06 22:35:01 +0530

    Refactors the calling of the popover functiopn in user js than in application js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,24 +2,25 @@
 // All this logic will automatically be available in application.js.
 
 function ready() {
-  $('.datepicker').datepicker();
-  userIsParentChangeHandler();
+    $('.datepicker').datepicker();
+    $('[data-toggle="popover"]').popover();
+    userIsParentChangeHandler();
 }
 
 function userIsParentChangeHandler() {
-  $('#user_is_parent').change(function () {
-    $(':input', '#kin').val('');
-    $(':input','#child').val('');
-    if ($(this).is(":checked")) {
-      $('#kin').hide();
-      $('#child').show();
-    } else {
-      $('#kin').show();
-      $('#child').hide();
-    }
-  });
+    $('#user_is_parent').change(function () {
+        $(':input', '#kin').val('');
+        $(':input', '#child').val('');
+        if ($(this).is(":checked")) {
+            $('#kin').hide();
+            $('#child').show();
+        } else {
+            $('#kin').show();
+            $('#child').hide();
+        }
+    });
 }
 
 (function () {
-  $(document).on('ready turbolinks:load', ready);
+    $(document).on('ready turbolinks:load', ready);
 }).call(this);
