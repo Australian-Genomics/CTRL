71903a0ba6a92dfac0a63d25264e03a4e37c2d7c
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-05-11 11:20:35 +0530

    Sets the default answer to be true in steps two and three and a hyper link in one of the descriptions in step two

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,13 @@
 class Question < ApplicationRecord
   enum answer: %w[false true not_sure]
+
+  def is_checked?(default_value)
+    if [11, 14].include? question_id
+      'false'
+    elsif answer == 'not_sure'
+      default_value
+    else
+      answer.eql? 'true'
+    end
+  end
 end
