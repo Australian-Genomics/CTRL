49b09adf04f079793a20228450c5d1b1fffe537b
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-05-14 08:04:07 +0530

    Sets up the default url options for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,6 +105,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = { :host => 'agha-canary.herokuapp.com' }
+  
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
