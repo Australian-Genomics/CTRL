171b73dd0f0a22972e78a9cdd416bea6fb5195c6
mohjay <mohinder@curvetomorrow.com.au>
2018-05-24 12:46:04 +0800

    rubocop refactor

--- app/lib/report_manager.rb
+++ app/lib/report_manager.rb
@@ -13,19 +13,9 @@ class ReportManager
   def self.daily_consent_changes_excel_file
     p = Axlsx::Package.new
     wb = p.workbook
-    styles = wb.styles
-    align_hash = { alignment: { horizontal: :center, vertical: :center, wrap_text: true } }
-    margins = { left: 0.4, right: 0.4, top: 0.3, bottom: 0.3, header: 0.3, footer: 0.3 }
-    setup = { fit_to_width: 1, fit_to_height: 20, orientation: :landscape, paper_size: 9 }
-    header_footer = { different_first: false, odd_footer: '&R&P of &N' }
-    wb.add_worksheet(name: 'Daily Consent Changes Export', page_margins: margins, page_setup: setup, header_footer: header_footer) do |sheet|
-      add_table_headers(align_hash, sheet, styles)
-      add_changes_data_rows(align_hash, sheet, styles)
-      sheet.column_widths(*COLUMNS_WIDTH)
-      wb.add_defined_name("'DailyConsentChanges'!$1:$11", local_sheet_id: sheet.index, name: '_xlnm.Print_Titles')
-    end
+    add_work_book_details(wb)
 
-    date_time_now_in_zone = Timezone['Australia/Melbourne'].time_with_offset(DateTime.now)
+    date_time_now_in_zone = Timezone['Australia/Melbourne'].time_with_offset(Time.now)
     today_date = date_time_now_in_zone.try(:strftime, '%d_%m_%Y')
 
     file_name_report = "#{Rails.root}/tmp/AGHA_Participant_Consent_Preference_Changes_#{today_date}.xlsx"
@@ -34,11 +24,33 @@ class ReportManager
     File.new(file_name_report)
   end
 
+  def self.add_work_book_details(wb)
+    styles = wb.styles
+    align_hash = { alignment: { horizontal: :center, vertical: :center, wrap_text: true } }
+    margins = { left: 0.4, right: 0.4, top: 0.3, bottom: 0.3, header: 0.3, footer: 0.3 }
+    setup = { fit_to_width: 1, fit_to_height: 20, orientation: :landscape, paper_size: 9 }
+    header_footer = { different_first: false, odd_footer: '&R&P of &N' }
+    add_work_sheet({ align_hash: align_hash, header_footer: header_footer, margins: margins, setup: setup, styles: styles }, wb)
+  end
+
+  def self.add_work_sheet(style_hash, wb)
+    wb.add_worksheet(name: 'Daily Consent Changes Export', page_margins: style_hash[:margins], page_setup: style_hash[:setup], header_footer: style_hash[:header_footer]) do |sheet|
+      add_table_headers(style_hash[:align_hash], sheet, style_hash[:styles])
+      add_changes_data_rows(style_hash[:align_hash], sheet, style_hash[:styles])
+      sheet.column_widths(*COLUMNS_WIDTH)
+      wb.add_defined_name("'DailyConsentChanges'!$1:$11", local_sheet_id: sheet.index, name: '_xlnm.Print_Titles')
+    end
+  end
+
   def self.add_changes_data_rows(align_hash, sheet, styles)
     field = styles.add_style align_hash.merge(border: { style: :thin, color: '000000' }, sz: 10)
     fields_styles = [field] * HEADERS.size
 
-    versions = Question.get_question_changes_for_last_day.sort_by(&:whodunnit)
+    versions = Question.question_changes_for_last_day.sort_by(&:whodunnit)
+    add_versions_to_sheet(fields_styles, sheet, versions)
+  end
+
+  def self.add_versions_to_sheet(fields_styles, sheet, versions)
     versions.each do |version|
       question = Question.find(version.item_id)
       step = question.step
@@ -48,10 +60,14 @@ class ReportManager
       event_time_in_zone = Timezone['Australia/Melbourne'].time_with_offset(version.created_at)
       version = event_time_in_zone.try(:strftime, '%d/%m/%Y %H:%M')
 
-      current_question = QUS.values.flatten.select { |x| x[:question_id] == question.question_id }.first[:qus]
-      data_row = [user.study_id, user.email, step.number, current_question, changes.first, changes.last, version]
+      data_row = create_data_row(changes, question, step, user, version)
       sheet.add_row(data_row, style: fields_styles, height: 30)
     end
+  end
+
+  def self.create_data_row(changes, question, step, user, version)
+    current_question = QUS.values.flatten.select { |x| x[:question_id] == question.question_id }.first[:qus]
+    [user.study_id, user.email, step.number, current_question, changes.first, changes.last, version]
   end
 
   def self.add_table_headers(align_hash, sheet, styles)
@@ -61,22 +77,17 @@ class ReportManager
     sub_header_left = styles.add_style sub_header_hash
     sub_header_right = styles.add_style sub_header_hash
 
-    custom_border = styles.borders[styles.cellXfs[sub_header_left].borderId]
-    custom_border.prs.each do |part|
-      case part.name
-      when :right
-        part.style = :thin
-      end
-    end
+    add_right_border(styles, sub_header_left)
 
-    custom_border = styles.borders[styles.cellXfs[sub_header_right].borderId]
-    custom_border.prs.each do |part|
-      case part.name
-      when :left
-        part.style = :thin
-      end
-    end
+    add_left_border(styles, sub_header_right)
 
+    add_end_borders(styles, sub_header_light)
+
+    headers_styles = [sub_header] * HEADERS.size
+    sheet.add_row(HEADERS, style: headers_styles, height: 25)
+  end
+
+  def self.add_end_borders(styles, sub_header_light)
     custom_border = styles.borders[styles.cellXfs[sub_header_light].borderId]
     custom_border.prs.each do |part|
       case part.name
@@ -86,10 +97,25 @@ class ReportManager
         part.style = :thin
       end
     end
+  end
 
-    header_columns = HEADERS
+  def self.add_left_border(styles, sub_header_right)
+    custom_border = styles.borders[styles.cellXfs[sub_header_right].borderId]
+    custom_border.prs.each do |part|
+      case part.name
+      when :left
+        part.style = :thin
+      end
+    end
+  end
 
-    headers_styles = [sub_header] * header_columns.size
-    sheet.add_row(header_columns, style: headers_styles, height: 25)
+  def self.add_right_border(styles, sub_header_left)
+    custom_border = styles.borders[styles.cellXfs[sub_header_left].borderId]
+    custom_border.prs.each do |part|
+      case part.name
+      when :right
+        part.style = :thin
+      end
+    end
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -9,7 +9,7 @@ class Question < ApplicationRecord
     answer.eql?('not_sure') ? default_value : answer.eql?('true')
   end
 
-  def self.get_question_changes_for_last_day
+  def self.question_changes_for_last_day
     PaperTrail::Version.where('item_type = ? AND event = ? AND created_at >= ?', 'Question', 'update', 1.day.ago)
   end
 end
