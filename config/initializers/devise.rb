4e11867eeb2b3294f0ecec3b3eafbb50c89eb906
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-05-30 15:50:55 +0530

    Sets the custom mailer in intializers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'ctrl@australiangenomics.org.au'
-
+  config.mailer = "CustomDeviseMailer"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
