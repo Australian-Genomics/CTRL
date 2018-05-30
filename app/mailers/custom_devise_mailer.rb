73a6f9f99db2cce13d870bd0b62a934403161fea
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-05-30 15:53:31 +0530

    Rubocop fix

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -3,7 +3,7 @@ class CustomDeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 
-  def reset_password_instructions(record, token, opts={})
+  def reset_password_instructions(record, token, opts = {})
     opts[:subject] = 'Reset Password Instructions'
     attach_image 'logo@2x.png'
     attach_image 'Rectangle.png'
@@ -15,5 +15,4 @@ class CustomDeviseMailer < Devise::Mailer
   def attach_image(image_name)
     attachments.inline[image_name] = File.read(File.join(Rails.root, 'app', 'assets', 'images', image_name))
   end
-
 end
