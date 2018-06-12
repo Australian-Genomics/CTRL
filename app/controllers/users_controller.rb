7fc8aa4dd914a29a649abbe878bccad121302757
Sushant Ahuja <sushant@curvetomorrow.com.au>
2018-06-12 15:12:35 +0530

    Rubocop fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :edit, :update]
+  before_action :find_user, only: %i[show edit update]
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
