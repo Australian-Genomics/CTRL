304ff3aa059aa4cd759f3124b795f640a41a1426
Sushant Ahuja <=>
2019-06-19 12:43:07 +0530

    Updates the sh files for setting up heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 git remote add heroku https://git.heroku.com/agha-canary.git
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_EMAIL
