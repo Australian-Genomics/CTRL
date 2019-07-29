5e36b26ba0eb36049cdf04a9d1826e7c05c2f7f8
Shailendra Prajapati <shailendra@curvetomorrow.com.au>
2019-07-30 00:52:36 +0530

    code optimization

--- app/lib/user_date_validator.rb
+++ app/lib/user_date_validator.rb
@@ -1,24 +1,19 @@
 module UserDateValidator
   def date_of_birth_in_future
-    return false unless date_valid?
+    return false unless date_valid?(:dob)
     !dob.future? ? true : errors.add(:dob, I18n.t('user.errors.dob.future')) && false
   end
 
   def child_date_of_birth_in_future
-    return unless is_parent == true
-    return false unless child_date_valid?
+    return false unless is_parent.eql?(true)
+    return false unless date_valid?(:child_dob)
     !child_dob.future? ? true : errors.add(:child_dob, I18n.t('user.errors.dob.future')) && false
   end
 
   private
 
-  def date_valid?
+  def date_valid?(dob)
     date_format = /\d{2}-\d{2}-(\d{4})$/
-    dob.to_s.match?(date_format) ? true : errors.add(:dob, I18n.t('user.errors.dob.invalid_format')) && false
-  end
-
-  def child_date_valid?
-    date_format = /\d{2}-\d{2}-(\d{4})$/
-    child_dob.to_s.match?(date_format) ? true : errors.add(:child_dob, I18n.t('user.errors.dob.invalid_format')) && false
+    send(dob).to_s.match?(date_format) ? true : errors.add(dob, I18n.t('user.errors.dob.invalid_format')) && false
   end
 end
