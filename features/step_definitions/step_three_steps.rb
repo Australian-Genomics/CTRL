cf817145aa4fdc77887e235e7ff41fe3341d9eb3
Sushant Ahuja <sushant@curvetomorrow.com.au>
2019-07-29 20:08:53 +0530

    Adds the step definition to click the last question on the step three

--- features/step_definitions/step_three_steps.rb
+++ features/step_definitions/step_three_steps.rb
@@ -6,6 +6,10 @@ Then('I should be on the step three page of consent') do
   expect(page).to have_content('Step 3 of 5')
 end
 
+Then('I click on Select All on step three') do
+  find('#selectCheckBox15 + span').click
+end
+
 Then('I should see the step four of the consent') do
   expect(page).to have_content('Step 4 of 5')
 end
