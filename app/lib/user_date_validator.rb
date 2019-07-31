3156348d979f8e0b9f4d369f50b45d9b6232278d
Shailendra Prajapati <shailendra@curvetomorrow.com.au>
2019-07-31 12:15:03 +0530

    code optimization

--- app/lib/user_date_validator.rb
+++ app/lib/user_date_validator.rb
@@ -5,8 +5,7 @@ module UserDateValidator
   end
 
   def child_date_of_birth_in_future
-    return false unless is_parent.eql?(true)
-    return false unless date_valid?(:child_dob)
+    return false unless is_parent.eql?(true) && date_valid?(:child_dob)
     !child_dob.future? ? true : errors.add(:child_dob, I18n.t('user.errors.dob.future')) && false
   end
 
