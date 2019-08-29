b870daff27c77dae9339bfc928bbc25878c2dd8d
Shailendra Prajapati <shailendra@curvetomorrow.com.au>
2019-08-29 17:42:59 +0530

    Updated scheduler

--- app/jobs/send_survey_emails.rb
+++ app/jobs/send_survey_emails.rb
@@ -1,7 +1,7 @@
 class SendSurveyEmails
   include Delayed::RecurringJob
   run_every 1.day
-  run_at '10:12pm'
+  run_at '10:16pm'
   timezone 'Australia/Melbourne'
   queue 'send_survey_emails'
   def perform
