538f10a3c320802c0f2d9b45be795586c6a45bc9
sd <deng_sharon@hotmail.com>
2019-10-23 10:23:25 +1100

    Load assets (#83)
    * SD: trial: pre-compile assets locally
    
    * SD: Fix JavaScript reload issue in pre-compile assets
    
    - menu bar active not working in assets pipeline due to only loading once
    
    * SD: Pre-compile assets for production

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,17 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-function ready() {
-    $('[data-toggle="popover"]').popover();
-    toggleNavBarLink();
-}
-
 function toggleNavBarLink() {
     $active_link = "#" + $('#active_tab').val();
     $('.navbar-nav').find('.active').removeClass('active');
     $('.navbar-nav').find($active_link).addClass('active');
 }
 
-(function () {
-    $(document).on('ready turbolinks:load', ready);
-}).call(this);
+$(document).on('page:change turbolinks:load', function(){
+    $('[data-toggle="popover"]').popover();
+    toggleNavBarLink();
+});
