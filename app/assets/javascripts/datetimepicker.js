f4e2a1b09d92a1f813f8ff7598e94a4ba04dc253
Nitin Srivastava <nitin@curvetomorrow.com>
2020-03-11 11:49:32 +0530

    Fixed calender default date

--- app/assets/javascripts/datetimepicker.js
+++ app/assets/javascripts/datetimepicker.js
@@ -1,7 +1,7 @@
 $(document).on('ready turbolinks:load', function() {
   $('#user_dob').datetimepicker({
     format: 'DD-MM-YYYY',
-    defaultDate: "01/01/1995",
+    useCurrent: false,
     icons: {
       previous: 'icon__prev',
       next: 'icon__next'
