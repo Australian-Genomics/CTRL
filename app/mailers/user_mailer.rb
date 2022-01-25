7a48b18c4023337a061347333ed9254a40e50433
max-hen <maxhdev79@gmail.com>
2022-01-26 07:16:43 +0800

    removes code related to User.send_daily_consent_email_to_matilda

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,18 +15,4 @@ class UserMailer < ApplicationMailer
   def attach_image(image_name)
     attachments.inline[image_name] = File.read(File.join(Rails.root, 'app', 'assets', 'images', image_name))
   end
-
-  def send_daily_consent_email_to_matilda
-    excel_file = ReportManager.daily_consent_changes_excel_file
-
-    date_time_now_in_zone = Timezone['Australia/Melbourne'].time_with_offset(Time.now)
-    @today_date = date_time_now_in_zone.try(:strftime, '%d_%m_%Y')
-    filename = "AGHA_Participant_Consent_Preference_Changes_#{@today_date}.xlsx"
-
-    attachments[filename] = { mime_type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', content: excel_file.read }
-
-    sender = 'ctrl@australiangenomics.org.au'
-    to_email = ENV['DAILY_CHANGES_EMAIL'] || 'aghatesting@gmail.com'
-    mail(to: "AGHA CTRL <#{to_email}>", subject: "AGHA Participant Consent Preference Changes for #{@today_date.tr('_', '/')}", sender: sender, from: sender)
-  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -18,16 +18,6 @@ class Question < ApplicationRecord
     end
   end
 
-  def self.question_changes_for_last_day
-    today_in_zone = Timezone['Australia/Melbourne'].time_with_offset(Time.now)
-    start_of_day = (today_in_zone - 1.day).beginning_of_day
-    end_of_day = (today_in_zone - 1.day).end_of_day
-    Question.left_outer_joins(:versions, step: :user)
-            .select('users.study_id, users.email, steps.number, questions.id, questions.question_id')
-            .group('users.study_id, users.email, steps.number, questions.id, questions.question_id')
-            .where('versions.event IN (?) AND versions.created_at >= ? AND versions.created_at < ?', %w[create update], start_of_day, end_of_day)
-  end
-
   def answer_value
     Question.answers[answer]
   end
