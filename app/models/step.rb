975a110e043079bd7fa6ea1c0aa2bca7d52d7950
max-hen <maxhdev79@gmail.com>
2022-01-27 03:57:04 +0800

    removes redcap related code

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,23 +1,8 @@
 class Step < ApplicationRecord
-  REDCAP_CONNECTED_STEPS = [4, 5].freeze
-
   has_many :questions, dependent: :destroy
   accepts_nested_attributes_for :questions
 
   belongs_to :user
 
   delegate :study_id, to: :user, prefix: true, allow_nil: true
-
-  def upload_with_redcap(step_params)
-    return unless update(step_params)
-    return unless redcap_connection_enabled?
-
-    UploadRedcapDetailsJob.perform_later(id)
-  end
-
-  private
-
-  def redcap_connection_enabled?
-    REDCAP_CONNECTED_STEPS.include?(number) && ENV['REDCAP_CONNECTION_ENABLED'].eql?('true')
-  end
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,8 +3,6 @@ require 'database_cleaner'
 require 'selenium-webdriver'
 require 'capybara-screenshot/cucumber'
 
-ENV['REDCAP_CONNECTION_ENABLED'] = 'false'
-
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
