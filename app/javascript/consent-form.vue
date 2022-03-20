c36fbb068bf27e6e8e1aea2bbbdc834f66393452
William Jones <will@sanogenetics.com>
2022-03-20 09:54:37 +0100

    README tweaks

--- app/javascript/consent-form.vue
+++ app/javascript/consent-form.vue
@@ -22,66 +22,87 @@
             }"
           >
 
-          <StepInitial
-            :iframeSrc="'stubStepURL'"
-            :surveyStep="currentSurveyStep"
-            v-if="isFirstStep && currentSurveyStep"
-          />
+            <StepInitial
+              :iframeSrc="'https://www.youtube.com/embed/Du8kYb_9AKY'"  
+              :surveyStep="currentSurveyStep"
+              v-if="isFirstStep && currentSurveyStep"
+            />
+            
 
-          <div v-if="consentStep !== 1">
-            <h3 class="text-center mt-2 mb-15">
-              {{ currentSurveyStep.title }}
-            </h3>
+            <div v-if="consentStep !== 1">
+              <h3 class="text-center mt-2 mb-15">
+                {{ currentSurveyStep.title }}
+              </h3>
 
-            <div class="d-flex mb-30">
-              <p class="steps__description mx-auto mb-0 text-justify"
-                v-html="currentSurveyStep.description">
-              </p>
-            </div>
-
-            <div
-              v-for="(qstionGrp, qGindex) in questionGroups"
-              v-bind:key="`qstionGrp-${qGindex}`"
-            >
-              <div class="steps__general text-center py-2 mb-30"
-                v-if="qstionGrp.header">
-                <b>{{qstionGrp.header}}</b>
+              <div class="d-flex mb-30">
+                <p class="steps__description mx-auto mb-0 text-justify"
+                  v-html="currentSurveyStep.description">
+                </p>
               </div>
-              <div class="options"
-                v-for="(question, i) in qstionGrp.questions"
-                v-bind:key="`question${question.id}`">
 
-                <div class="steps__row">
-                  <div class="steps__row-main">
-                    <div class="mr-auto steps__text-question"
-                      v-html="question.question">
+              <div
+                v-for="(qstionGrp, qGindex) in questionGroups"
+                v-bind:key="`qstionGrp-${qGindex}`"
+              >
+                <div class="steps__general text-center py-2 mb-30"
+                  v-if="qstionGrp.header">
+                  <b>{{qstionGrp.header}}</b>
+                </div>
+                <div class="options"
+                  v-for="(question, i) in qstionGrp.questions"
+                  v-bind:key="`question${question.id}`">
+
+                  <div class="steps__row">
+                    <div class="steps__row-main">
+                      <div class="mr-auto steps__text-question"
+                        v-html="question.question">
+                      </div>
+
+                      <i
+                        class="icon__i mr-30 mr-sm-60"
+                        v-on:click="toggleDescription(question)"
+                        v-if="question.description"
+                      >
+                      </i>
+
+                      <template v-if="question.answer_choices_position === 'right'">
+                        <label class="controls controls__checkbox controls__checkbox_blue"
+                          v-if="question.question_type == 'checkbox' || question.question_type == 'checkbox agreement'">
+
+                          <input
+                            v-model="answers[qGindex][i].answer"
+                            type="checkbox"
+                            true-value="yes"
+                            false-value="no"
+                          >
+                          <span class="checkmark">
+                          </span>
+                        </label>
+
+                        <div
+                          class="d-flex mt-3 mt-sm-0 text-capitalize"
+                          v-else-if="question.question_type === 'multiple choice'"
+                        >
+                          <span v-for="(option, oi) in question.options">
+                            <label>
+                              <label class="controls controls__radio controls__radio_blue ml-sm-15">
+                                <input type="radio"
+                                  v-bind:value="option.value"
+                                  v-model="answers[qGindex][i].answer"
+                                >
+                                <span class="checkmark"></span>
+                                <span class="label-text ml-1 mr-1">{{ option.value }}</span>
+                              </label>
+                            </label>
+                          </span>
+                        </div>
+                      </template">
                     </div>
 
-                    <i
-                      class="icon__i mr-30 mr-sm-60"
-                      v-on:click="toggleDescription(question)"
-                      v-if="question.description"
-                    >
-                    </i>
-
-                    <template v-if="question.answer_choices_position === 'right'">
-                      <label class="controls controls__checkbox controls__checkbox_blue"
-                        v-if="question.question_type == 'checkbox' || question.question_type == 'checkbox agreement'">
-
-                        <input
-                          v-model="answers[qGindex][i].answer"
-                          type="checkbox"
-                          true-value="yes"
-                          false-value="no"
-                        >
-                        <span class="checkmark">
-                        </span>
-                      </label>
-
-                      <div
-                        class="d-flex mt-3 mt-sm-0 text-capitalize"
-                        v-else-if="question.question_type === 'multiple choice'"
-                      >
+                    <div class="col-12"
+                      v-if="question.question_type === 'multiple choice' &&
+                        question.answer_choices_position === 'bottom'">
+                      <div class="d-flex mt-3 mt-sm-0 text-capitalize">
                         <span v-for="(option, oi) in question.options">
                           <label>
                             <label class="controls controls__radio controls__radio_blue ml-sm-15">
@@ -95,67 +116,47 @@
                           </label>
                         </span>
                       </div>
-                    </template">
-                  </div>
-
-                  <div class="col-12"
-                    v-if="question.question_type === 'multiple choice' &&
-                      question.answer_choices_position === 'bottom'">
-                    <div class="d-flex mt-3 mt-sm-0 text-capitalize">
-                      <span v-for="(option, oi) in question.options">
-                        <label>
-                          <label class="controls controls__radio controls__radio_blue ml-sm-15">
-                            <input type="radio"
-                              v-bind:value="option.value"
-                              v-model="answers[qGindex][i].answer"
-                            >
-                            <span class="checkmark"></span>
-                            <span class="label-text ml-1 mr-1">{{ option.value }}</span>
-                          </label>
-                        </label>
-                      </span>
                     </div>
                   </div>
-                </div>
-                <div class="steps__row-collapse"
-                  v-if="question.show_description && question.description">
-                  <div class="steps__text-question"
-                    v-html="question.description">
+                  <div class="steps__row-collapse"
+                    v-if="question.show_description && question.description">
+                    <div class="steps__text-question"
+                      v-html="question.description">
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div class="row flex-md-row">
-            <div class="col-6 col-md-3 order-0 d-flex my-15 my-md-30">
-              <button
-                v-on:click="previousStep"
-                v-if="consentStep !== 1"
-                class="btn btn-outline-success mr-auto"
-              >
-                Back
-              </button>
-            </div>
+            <div class="row flex-md-row">
+              <div class="col-6 col-md-3 order-0 d-flex my-15 my-md-30">
+                <button
+                  v-on:click="previousStep"
+                  v-if="consentStep !== 1"
+                  class="btn btn-outline-success mr-auto"
+                >
+                  Back
+                </button>
+              </div>
 
-            <div class="col-6 col-md-3 order-1 order-md-2 d-flex my-15 my-md-30">
-              <button
-                v-on:click="nextStep"
-                v-if="consentStep !== consentStepTotal"
-                class="btn btn-success ml-auto"
-              >
-                Next
-              </button>
-            </div>
+              <div class="col-6 col-md-3 order-1 order-md-2 d-flex my-15 my-md-30">
+                <button
+                  v-on:click="nextStep"
+                  v-if="consentStep !== consentStepTotal"
+                  class="btn btn-success ml-auto"
+                >
+                  Next
+                </button>
+              </div>
 
-            <div class="col-12 col-md-6 order-2 order-md-1 d-flex mb-30 mt-md-30">
-              <button class="steps__btn-save btn btn-primary mx-auto"
-                v-on:click="saveAndExit"
-              >
-                Save and Exit
-              </button>
+              <div class="col-12 col-md-6 order-2 order-md-1 d-flex mb-30 mt-md-30">
+                <button class="steps__btn-save btn btn-primary mx-auto"
+                  v-on:click="saveAndExit"
+                >
+                  Save and Exit
+                </button>
+              </div>
             </div>
-          </div>
           </div>
         </div>
       </div>
