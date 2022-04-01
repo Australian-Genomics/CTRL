a255753725c397a87d694597718525568f9d0dd5
Muhammad Abid <abid.iqbal@ssidecisions.com>
2022-04-01 17:37:24 +0500

    configuring active admin with webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const jquery = require('./plugins/jquery')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('jquery', jquery)
 module.exports = environment
