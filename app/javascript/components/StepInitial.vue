01960ad6a614bbac2b1d8a96330fc1cd9a934444
Muhammad Abid <abid.iqbal@ssidecisions.com>
2022-04-06 23:22:41 +0500

    adding dynamic embedded links via admin for consent step 1

--- app/javascript/components/StepInitial.vue
+++ app/javascript/components/StepInitial.vue
@@ -7,14 +7,34 @@
     </div>
 
     <div class="steps__iframe">
-      <iframe
-        width="100%"
-        height="100%"
-        :src="iframeSrc"
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-      ></iframe>
+      <div id="carouselIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item " v-for="(video, index) in surveyStep.tour_videos"
+               v-bind:key="`tourVideo-${index}`" v-bind:class="{ active: index===0 }">
+            <iframe
+                width="100%"
+                height="100%"
+                :src="video"
+                frameborder="0"
+                allow="autoplay; encrypted-media"
+                allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_192359) do
+ActiveRecord::Schema.define(version: 2022_04_05_143557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_01_17_192359) do
     t.text "popover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "tour_videos"
     t.index ["order"], name: "index_consent_steps_on_order", unique: true
   end
 
