1f061d335b570f2d758999b545f67c685d94b9a3
Muhammad Abid <mabidiqbalk@gmail.com>
2022-05-18 08:50:55 +0500

    wip on password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   mcri_email = {
       address: "smtp.mcri.edu.au",
