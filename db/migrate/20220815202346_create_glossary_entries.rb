abe1a94e5f603a8aaf946c885112e0baac519798
Christian Nassif-Haynes <ch.na.ha@gmail.com>
2022-09-07 10:27:19 +1000

    Make Glossary columns not null (#53)

--- db/migrate/20220815202346_create_glossary_entries.rb
+++ db/migrate/20220815202346_create_glossary_entries.rb
@@ -1,8 +1,8 @@
 class CreateGlossaryEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :glossary_entries do |t|
-      t.string :term
-      t.text :definition
+      t.string :term, null: false
+      t.text :definition, null: false
 
       t.timestamps
     end
